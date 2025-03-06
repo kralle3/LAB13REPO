938b80b33359d936f3680f15a1b07f0eaecba51b
kralle3 <kamarirallen@gmail.com>
2025-03-06 15:34:34 -0600

    Add files via upload

--- Lab13Repo.py
+++ Lab13Repo.py
@@ -4,6 +4,8 @@ import random
 led = LED(21)
 right_button = Button(16)
 left_button = Button(20)
+left_score = 0
+right_score = 0
 left_name = ("Player1")
 right_name= ('PLayer2')
 
@@ -11,13 +13,15 @@ right_name= ('PLayer2')
 def pressed(button):
     global right_name
     global left_name
+    global left_score
+    global right_score
     if button.pin.number == 20:
         print(left_name + 'pressed first!')
-       
+        left_score+=1
         led.off()
     elif button.pin.number == 16:
         print(right_name + ' pressed first!')
-        
+        right_score+=1
         led.off()
         
 
@@ -26,5 +30,5 @@ for i in range (0, 10):
     led.on()
     right_button.when_pressed = pressed
     left_button.when_pressed = pressed
-    
+    print(' Score is '+ (str(left_score)) + ' - ' + (str(right_score)))
     
